cf8b1bd93774a06af5c137bbedc8ef7314cc87f6
Tomas <cristiantomasvegaappelqvist@gmail.com>
2026-05-22 13:11:49 +0100

    FireBase Linked

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "ENTER_YOUR_API_KEY_HERE",
-    authDomain: "YOUR_PROYECT.firebaseapp.com",
-    projectId: "YOUR_PROYECT_ID",
-    storageBucket: "YOUR_DB.firebasestorage.app",
-    messagingSenderId: "YOUR_MESSAGING_SENDER",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID" // It's optional (It's for Google Analytics)
+    apiKey: "AIzaSyAqmWJgUk31CMGPgaDTdHCQOuVuY4bskFw",
+    authDomain: "wishdish-6ae6a.firebaseapp.com",
+    projectId: "wishdish-6ae6a",
+    storageBucket: "wishdish-6ae6a.firebasestorage.app",
+    messagingSenderId: "219625207924",
+    appId: "1:219625207924:web:090e6a9d0c05a650947921",
+    measurementId: "G-96YWQ8PY86"
   }
 };
