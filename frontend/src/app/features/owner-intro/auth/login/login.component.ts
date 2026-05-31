d59ae03effc4a9f3fccee31563bffa283a5c1f0d
Tomas <cristiantomasvegaappelqvist@gmail.com>
2026-06-01 00:38:41 +0100

    Login finished

--- frontend/src/app/features/owner-intro/auth/login/login.component.ts
+++ frontend/src/app/features/owner-intro/auth/login/login.component.ts
@@ -3,38 +3,38 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../../core/services/owner-intro/auth.service';
 import { Router } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, TranslateModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
-  isRegistering: boolean = false; // Para alternar entre vista de login y registro
   errorMessage: string = '';
+  showPassword: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) {}
 
   onSubmit() {
-    if (this.isRegistering) {
-      this.authService.register(this.email, this.password)
-        .then(() => {
-          console.log('¡Registrado con éxito!');
-          // Si tiene éxito, lo mandamos a la pantalla de los 4 botones
-          this.router.navigate(['/owner-intro']);
-        })
-        .catch(err => this.errorMessage = err.message);
-    } else {
-      this.authService.login(this.email, this.password)
-        .then(() => {
-          console.log('¡Logueado con éxito!');
-          this.router.navigate(['/owner-intro']);
-        })
-        .catch(err => this.errorMessage = 'Credenciales incorrectas');
-    }
+    this.authService.login(this.email, this.password)
+      .then(() => {
+        console.log('¡Logueado con éxito!');
+        this.router.navigate(['/owner-intro']); // O la ruta principal que decidas luego
+      })
+      .catch(err => this.errorMessage = 'Credenciales incorrectas');
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  goToRegister() {
+    // Redirige a la futura pantalla de registro
+    this.router.navigate(['/owner-intro/register']);
   }
 }
