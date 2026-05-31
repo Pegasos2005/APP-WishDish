29fa831b0f9833e473b9ef2a5e3d4188a2d26b4c
Tomas <cristiantomasvegaappelqvist@gmail.com>
2026-05-31 20:17:05 +0100

    Implementing Languajes in header

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 
 // Importaciones de Firebase
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+
+// Importaciones nuevas para Idiomas
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// Función mágica que le dice a Angular dónde buscar los JSON
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,6 +27,17 @@ export const appConfig: ApplicationConfig = {
     // v ============== Firebase =============== v
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()) // Permite hacer peticiones HTTP (GET, POST...) desde cualquier componente al backend
+    provideFirestore(() => getFirestore()), // Permite hacer peticiones HTTP (GET, POST...) desde cualquier componente al backend
+
+    // Encendemos el Traductor
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    )
   ]
 };
