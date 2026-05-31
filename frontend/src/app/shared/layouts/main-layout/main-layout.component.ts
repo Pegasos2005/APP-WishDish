acfb0e2e9e28e593435c500e069ccbd371b3a71d
Tomas <cristiantomasvegaappelqvist@gmail.com>
2026-05-31 20:26:15 +0100

    header adding

---
+++ frontend/src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component'; // Asegúrate de que la ruta sea correcta
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent], // <-- Súper importante añadirlos aquí
+  templateUrl: './main-layout.component.html',
+  styleUrls: ['./main-layout.component.css']
+})
+export class MainLayoutComponent { }
